b57e391e8f098204beefbf84de595007fb1c3090
Russ738 <russjwelch@gmail.com>
2026-04-11 17:10:01 -0400

    add observation type

--- gym_core/observation.py
+++ gym_core/observation.py
@@ -1,5 +1,6 @@
 from typing import TypedDict
-from gym_core.player import PlayerDict, Player
+from gym_core.player import PlayerDict
+
 class Observation(TypedDict):
     """player 0 is always the agent"""
     player_dict: PlayerDict
